eae1bcd15f8f45fa661e104cc41bf5e9e33943d9
Altair <46060005+RebAltair@users.noreply.github.com>
2021-11-20 21:51:43 +0800

    Revert "Create the most basic structure of LoginServer"

--- src/LoginServer/ProcessingUnit/registration.go
+++
@@ -1,9 +0,0 @@
-package ProcessingUnit
-
-import "github.com/gin-gonic/gin"
-
-func Registration(Username string, Password string, c **gin.Context) {
-	(*c).JSON(200, gin.H{
-		"Data": "Under Development",
-	})
-}
