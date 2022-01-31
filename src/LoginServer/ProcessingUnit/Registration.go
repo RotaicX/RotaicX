ca2fd18bf84f1e84f9f3ffe433be6ee7bc9e40a8
rebaltair <2860068369@qq.com>
2022-01-31 17:46:23 +0800

    修复了巨大的结构问题

--- src/LoginServer/ProcessingUnit/Registration.go
+++ src/LoginServer/ProcessingUnit/Registration.go
@@ -6,6 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Registration(Username interface{}, Password interface{}, c **gin.Context) {
+func Registration(Username interface{}, Password interface{}, c *gin.Context) {
 	ReturnData.Code200(fmt.Sprintf("Under Development, You Username %s, Password %s", Username, Password), c)
 }
