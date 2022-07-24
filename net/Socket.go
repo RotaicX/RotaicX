b13ac9a759b9703687a1665952b5f26f1da4b17e
rebaltair <2860068369@qq.com>
2022-07-24 17:44:10 +0800

    Add Rsa Encryption Algorithm Support

--- net/Socket.go
+++ net/Socket.go
@@ -2,7 +2,6 @@ package net
 
 import (
 	"github.com/xtaci/kcp-go/v5"
-	"net"
 	"strconv"
 )
 
@@ -17,12 +16,15 @@ func InitKCP(Addr string) (*kcp.Listener, error) {
 	return listener, nil
 }
 
-func GetData(conn net.Conn) (string, error) {
+// GetData () Read the data from net.Conn and get the body content according to the protocol format
+func GetData(conn *kcp.UDPSession) (string, error) {
 	dataLength := make([]byte, 8)
+
 	_, err := conn.Read(dataLength)
 	if err != err {
 		return "", err
 	}
+
 	for _, v := range dataLength {
 		if v == '0' {
 			dataLength = dataLength[1:]
@@ -30,15 +32,34 @@ func GetData(conn net.Conn) (string, error) {
 			break
 		}
 	}
+
 	sDataLen := string(dataLength)
 	iDataLen, err := strconv.Atoi(sDataLen)
 	if err != nil {
 		return "", err
 	}
+
 	data := make([]byte, iDataLen)
+
 	_, err = conn.Read(data)
 	if err != nil {
 		return "", err
 	}
+
 	return string(data), nil
+}
+
+func MakeData(Data string) []byte {
+	bData := []byte(Data)
+	iDataLen := len(bData)
+	sDataLen := strconv.Itoa(iDataLen)
+	if len(sDataLen) != 8 {
+		complement := 8 - len(sDataLen)
+		for l := 0; l != complement; l++ {
+			sDataLen = "0" + sDataLen
+		}
+	}
+
+	bAllData := append([]byte(sDataLen), bData...)
+	return bAllData
 }
